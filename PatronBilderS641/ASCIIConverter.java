15f9016728006366724d82af1c29bd8540eceedb
Juan Camilo Tenorio <168867884+Detesree@users.noreply.github.com>
2025-09-19 14:50:13 +0000

    Creacion de las clases para patron Builder

---
+++ PatronBilderS641/ASCIIConverter.java
@@ -0,0 +1,16 @@
+public class ASCIIConverter extends Converter{
+     public void makeLine(){
+        System.out.println("Line con ASCII");
+
+    }
+
+    public void makeParagraph(){
+        System.out.println("Parrafo con ASCII");
+
+    }
+
+    public void makeTable(){
+        System.out.println("Tabla con ASCII");
+
+    }
+}
---
+++ PatronBilderS641/Reader.java
@@ -0,0 +1,26 @@
+public class Reader {
+    private String element;
+    private Converter converter;
+
+    public Reader(String element, Converter converter){
+        this.element = element;
+        this.converter = converter;
+    }
+    //Metodos
+    public void parseInput(){
+        switch (element) {
+            case "LINE":
+            converter.makeLine();
+                
+                break;
+            case "PARAGRAPH":
+            converter.makeParagraph();
+            case "TABLE":
+            
+                
+        
+            default:
+                break;
+        }
+    }
+}

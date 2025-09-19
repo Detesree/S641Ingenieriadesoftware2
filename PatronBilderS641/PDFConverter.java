bb07ead3abb89be1b6a17ac2da4378985701d41e
Juan Camilo Tenorio <168867884+Detesree@users.noreply.github.com>
2025-09-19 14:50:13 +0000

    Creacion de las clases para patron Builder

---
+++ PatronBilderS641/PDFConverter.java
@@ -0,0 +1,16 @@
+public class PDFConverter extends Converter {
+         public void makeLine(){
+        System.out.println("Line con PDF");
+
+    }
+
+    public void makeParagraph(){
+        System.out.println("Parrafo con PDF");
+
+    }
+
+    public void makeTable(){
+        System.out.println("Tabla con PDF");
+
+    }
+}
---
+++ PatronBilderS641/PostScriptConverter.java
@@ -0,0 +1,16 @@
+public class PostScriptConverter extends Converter{
+     public void makeLine(){
+        System.out.println("Line con PostScript");
+
+    }
+
+    public void makeParagraph(){
+        System.out.println("Parrafo con PostScript");
+
+    }
+
+    public void makeTable(){
+        System.out.println("Tabla con PostScript");
+
+    }
+}
